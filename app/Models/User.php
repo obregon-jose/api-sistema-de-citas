592b54bca4cf2ff02a02b23165e2408ef552fc2d
obregon-jose <jo46646@gmail.com>
2024-09-17 23:04:21 -0500

    Revert "Roles migracion y sedeer, funcion para registrar usuario"
    This reverts commit dc993ba6613e930207e5c6293a4912626d7155b3.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
@@ -12,18 +11,6 @@ use Laravel\Passport\HasApiTokens;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens;
-
-    // public function hasRole($roleName)
-    // {
-    //     return $this->profiles()->whereHas('role', function($query) use ($roleName) {
-    //         $query->where('name', $roleName);
-    //     })->exists();
-    // }
-
-    // public function profiles()
-    // {
-    //     return $this->hasMany(Profile::class);
-    // }
 
     /**
      * The attributes that are mass assignable.
@@ -58,6 +45,4 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
-
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        
-        $this->call(RoleSedeer::class);
-        // $this->call(UserSedeer::class);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
