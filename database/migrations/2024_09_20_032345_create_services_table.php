105eb2881bf7d4f7833a71dc055c37b9e2654b63
obregon-jose <jo46646@gmail.com>
2024-10-15 10:35:37 -0500

    Refactor service

--- database/migrations/2024_09_20_032345_create_services_table.php
+++ database/migrations/2024_09_20_032345_create_services_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->integer('price')->nullable();
+            $table->integer('price');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -45,74 +45,67 @@ Route::get('/', function () {
 });
 
 // RUTAS PUBLICAS (No requieren autenticación)
-Route::post('password/send-reset-code', [PasswordResetController::class, 'sendResetCode']);
-Route::post('password/verify-reset-code', [PasswordResetController::class, 'verifyResetCode']);
-Route::post('password/reset/update', [PasswordResetController::class, 'updatePassword']);
+Route::group(['prefix' => '/',], function () {
+    Route::post('password/send-reset-code', [PasswordResetController::class, 'sendResetCode']);
+    Route::post('password/verify-reset-code', [PasswordResetController::class, 'verifyResetCode']);
+    Route::post('password/reset/update', [PasswordResetController::class, 'updatePassword']);
 
-// limitar intentos de registro y login con middleware 'throttle' indicando los intentos permitidos,tiempo 
-Route::post('/users', [UserController::class, 'store'])->middleware('throttle:5,1'); // se registra como cliente
-Route::post('/login', [LoginController::class, 'login'])->middleware('throttle:10,1');
+    // limitar intentos de registro y login con middleware 'throttle' indicando los intentos permitidos,tiempo 
+    Route::post('/users', [UserController::class, 'store'])->middleware('throttle:5,1'); // se registra como cliente
+    Route::post('/login', [LoginController::class, 'login'])->middleware('throttle:10,1');
+});
 
+// ------------------AUTENTICACIÓN REQUERIDA
 // Perfil del usuario autenticado
 Route::get('/user', function (Request $request) {
     $user = $request->user()->load(['profiles.role', 'detail']);
     return $user;
 })->middleware('auth:api');
 
-// Grupo de rutas que solo requieren autenticación (Todos los roles)
 Route::group(['middleware' => ['auth:api']], function () {
-    
+/* ---------------- SOLO USUARIOS AUTENTICADOS --------------------*/
     Route::post('/logout', [LogoutController::class, 'logout']);
-    
-    // rutas usuarios
+    // usuarios
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::put('/users/{id}', [UserController::class, 'update']); //revisar - pacth
-    
-    // rutas servicios
+    // servicios
     Route::get('/services',[ServiceController::class,'index']);
 
-    //rutas detalles de usuario
+    // detalles de usuario
     // Route::get('/user-details', [UserDetailController::class, 'index']); //NO, se obtiene desde users
     // Route::post('/user-details', [UserDetailController::class, 'store']); //NO, se crea desde users
     // Route::get('/user-details/{id}', [UserDetailController::class, 'show']); //NO, se obtiene desde users
     Route::put('/user-details/{id}', [UserDetailController::class, 'update']);
     //Route::delete('/user-details/{id}', [UserDetailController::class, 'destroy']); //NO, eliminación en cascada desde users
 
-});
+    /* ---------------- RUTAS CON ROLES --------------------*/
+    // Requieren el rol 'cliente'
+    Route::group(['middleware' => [ CheckRole::class . ':cliente']], function () {
+        
+    });
+    // Requieren el rol 'peluquero'
+    Route::group(['middleware' => [ CheckRole::class . ':peluquero']], function () {
+        // rutas servicios
+        Route::get('/services/{id}',[ServiceController::class,'show']); 
+        Route::post('/services',[ServiceController::class,'store']);
+        Route::put('/services/{id}',[ServiceController::class,'update']);
+        Route::delete('/services/{id}',[ServiceController::class,'destroy']);
 
-
-
-// Grupo de rutas que requieren el rol 'root'
-Route::group(['middleware' => ['auth:api', CheckRole::class . ':root']], function () {
-    // rutas usuarios
-    
-    Route::get('/users', [UserController::class, 'index']); //solo con permisos
-    Route::delete('/users/{id}', [UserController::class, 'destroy']); // revisar
-    Route::post('/register', [RegisterController::class, 'register'])->middleware('throttle:5,1');  //puede seleccionar rol
-    
-});
-
-// Grupo de rutas que requieren el rol 'cliente'
-Route::group(['middleware' => ['auth:api', CheckRole::class . ':cliente']], function () {
-    
-    
-});
-
-// Grupo de rutas que requieren el rol 'peluquero' ++root
-Route::group(['middleware' => ['auth:api', CheckRole::class . ':peluquero']], function () {
-    // rutas servicios
-    Route::get('/services/{id}',[ServiceController::class,'show']); 
-    Route::post('/services',[ServiceController::class,'store']);
-    Route::put('/services/{id}',[ServiceController::class,'update']);
-    Route::delete('/services/{id}',[ServiceController::class,'destroy']);
-
-});
-
-// ADICIONALES
-Route::group(['middleware' => ['auth:api', CheckRole::class . ':administrador']], function () {
-    
-});
-
-Route::group(['middleware' => ['auth:api', CheckRole::class . ':dueño']], function () {
-    
+    });
+    // Requieren el rol 'administrador'
+    Route::group(['middleware' => [CheckRole::class . ':administrador']], function () {
+        
+    });
+    // Requieren el rol 'dueño'
+    Route::group(['middleware' => [CheckRole::class . ':dueño']], function () {
+        
+    });
+    // Requieren el rol 'root'
+    Route::group(['middleware' => [CheckRole::class . ':root']], function () {
+        // rutas usuarios
+        Route::get('/users', [UserController::class, 'index']); //solo con permisos
+        Route::delete('/users/{id}', [UserController::class, 'destroy']); // revisar
+        Route::post('/register', [RegisterController::class, 'register'])->middleware('throttle:5,1');  //puede seleccionar rol
+        
+    });
 });
