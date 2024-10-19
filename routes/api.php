2c1b5a454cd10774172620a5c5344e4395a24a9f
valentina0827 <deyci.otero@correounivalle.edu.co>
2024-10-19 10:20:10 -0500

    n

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
                 let interval = setInterval(function() {
                     progress += 1;
                     if (progress <= 100) {
-                        cargaElement.innerText = 'Cargando... ' + progress + '%';
+                        cargaElement.innerText = 'nnnn... ' + progress + '%';
                     } else {
                         clearInterval(interval);
                         messageElement.innerText = 'Hackeo completado';
