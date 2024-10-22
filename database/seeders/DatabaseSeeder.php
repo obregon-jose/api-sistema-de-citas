e99f34fcfb1bc7402ce0eeb2db1ef7fe8fbc8fc8
obregon-jose <jo46646@gmail.com>
2024-10-22 14:46:44 -0500

    Add more seeders to DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-        ]);
+        ]); // Add more seeders here
     }
 }
